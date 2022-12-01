935a4a73776e0acf3bc6e2c12ad3b01cec0dfd4e
Neel Patel <neelp@hotmail.co.uk>
2022-12-01 16:33:09 +0000

    Day 1

--- Day 1/Day 1 A.py
+++ Day 1/Day 1 A.py
@@ -1,17 +1,24 @@
+# Keep track of current and maximum calorie counts.
 cur_cal = 0
 max_cal = 0
 
+# Open inputs file.
 with open(r'D:\Documents\Programming\Python Projects\Programming Challenges\Adventure of Code\Day 1\input.txt', 'r') as file:
 
+    # Read through all file lines.
     lines = file.readlines()
 
+    # Iterate through each line in text file.
     for line in lines:
 
+        # If the line is blank, reset calorie counter.
         if line == '\n':
             cur_cal = 0
         else:
+            # Keep on adding calories.
             cur_cal += int(line)
 
+        # If the current calories is greater than any previous elf update.
         if cur_cal > max_cal:
             max_cal = cur_cal
 
--- Day 1/Day 1 B.py
+++ Day 1/Day 1 B.py
@@ -1,17 +1,25 @@
+# Keep a list of calories for each elf as well as counter.
 cur_cal = 0
 cal_list = []
 
+# Open text file.
 with open(r'D:\Documents\Programming\Python Projects\Programming Challenges\Adventure of Code\Day 1\input.txt', 'r') as file:
 
+    # Read all lines in text file.
     lines = file.readlines()
 
+    # Iterate through each line.
     for line in lines:
 
+        # If the line is blank add the calories to the list for that elf and reset counter.
         if line == '\n':
             cal_list.append(cur_cal)
             cur_cal = 0
         else:
+            # Keep on adding calories.
             cur_cal += int(line)
 
+# Sort the calories list.
 sorted_cal = sorted(cal_list)
+# Sum the largest 3.
 print(sum(sorted_cal[-3:]))
